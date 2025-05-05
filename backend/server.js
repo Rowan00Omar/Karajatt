2f9790dcaadb562d587533b8a7451c60af3ac7ec
toselk <toselk4@gmail.com>
2025-05-06 01:18:28 +0300

    user auth and navbar comp

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+require("dotenv").config();
+const authRoutes = require("./auth/authRoutes");
 
-app.listen(4000, () => {
-  console.log("Listening to port 4000");
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
