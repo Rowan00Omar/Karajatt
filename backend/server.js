b152da3648bf96c1ca89516701afb84a19700ad0
toselk <toselk4@gmail.com>
2025-06-17 18:51:14 +0300

    s

--- backend/server.js
+++ backend/server.js
@@ -107,7 +107,7 @@ async function initServer() {
 
     // Start server
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (error) {
