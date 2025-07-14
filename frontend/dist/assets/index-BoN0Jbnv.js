7e69ab2924eb488fd5fc65d2a4eb63bcc623c62b
ali <toselk21@gmail.com>
2025-07-14 19:44:57 +0300

    a

--- frontend/dist/assets/index-BoN0Jbnv.js
+++
@@ -1,1 +0,0 @@
-import{f as r,i as t}from"./index-3O0dXRsw.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ frontend/dist/assets/index-ChClsMVP.js
@@ -0,0 +1,1 @@
+import{f as r,i as t}from"./index-Bcnj4llk.js";var a=t();const e=r(a);export{e as R,a as r};
